001040bd3954f6b954875567647c042cf3d6ef8b
kubkowski <jakub.opara@gmail.com>
2016-12-14 18:30:27 +0100

    Initial repository

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :redmine_time_verification do
+  name 'Redmine Time Verification plugin'
+  author 'Jakub Opara'
+  description 'Simple plugin extending time verification method in TimeEntry model.'
+  version '0.0.1'
+  url 'http://example.com/path/to/plugin'
+  author_url 'http://example.com/about'
+end
